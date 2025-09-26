8742268cb5a91aa5173d94eef98a365a8b672b0e
jbdorey <jbdorey@me.com>
2025-09-26 15:26:09 +1000

    Update beeHotel_app.py

--- beeHotel_app.py
+++ beeHotel_app.py
@@ -11,7 +11,7 @@ import dropbox
 
     # Set up the navigation bar
         # icons at https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded&icon.size=24&icon.color=%23e3e3e3
-dashPage = st.Page("pages/Dashboard.py", title="Dashboard", icon = ":material/home:")
+dashPage = st.Page("pages/Dashboard.py", title="Dashboard", icon = ":material/home:", default=True)
 portalPage = st.Page("pages/1_Data portal.py", title="Data portal", icon = ":material/database_upload:")
 installPage = st.Page("pages/5_Hotel installation.py", title="Install your hotel", icon = ":material/handyman:")
 CheckPage = st.Page("pages/4_Checking your hotel.py", title="Check your hotel", icon = ":material/mystery:")
