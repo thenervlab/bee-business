ea31c930c69632e8f6a8d502cd509635034480d7
jbdorey <jbdorey@me.com>
2025-09-30 22:05:26 +1000

    Update 4_Checking your hotel.py

--- pages/4_Checking your hotel.py
+++ pages/4_Checking your hotel.py
@@ -5,15 +5,11 @@ st.title("Making bee hotel observations")
 st.subheader("How and when to check your hotel?")
 st.write("""
 To check your observation hotel, you will need to open the hotel by removing the curtain cord from the hotel and removing the lid panel.
+
+It would be great if the hotel/s could be checked once a day, in the evening (when it is dark and the bees are within the hotel). If you are super keen, you could check twice a day, preferably once in the middle of the day and once in the evening. And, keeping in mind that this may be too strenuous for many people, a maximum interval of one week might be acceptable. But, daily is ideal for a better resolution understanding of our native bee's biology!
 """)
 st.write("""
-It would be great if the hotel/s could be checked once a day, in the evening (when it is dark and the bees are within the hotel). If you are super keen, you could check twice a day, preferably once in the middle of the day and once in the evening. 
-""")
-st.write("""
-For each observation you need to take a clear photograph that is time stamped with the hotel code and the nest tunnel letter clearly visible. This will allow us to identify the number of tunnels, size of the nest hole used, brood production, false cells, sex ratios, activity window of the species and the unique identifier code of the hotel. 
-""")
-st.write("""
-The photograph and data will then need to be uploaded to the data portal.
+For each observation you need to take a clear photograph that is time stamped with the hotel code and the nest tunnel letter clearly visible. This will allow us to identify the number of tunnels, size of the nest hole used, brood production, false cells, sex ratios, activity window of the species and the unique identifier code of the hotel. The photograph and data will then need to be uploaded to the data portal.
 """)
 st.write("""
 We do not want the task of checking and uploading the data to be onerous, and we realise that there may be periods where you are away and can not check the hotel. This is absolutely fine, please just check it as regularly as you are able (keeping in mind this is a maximum of twice per day). 
@@ -21,33 +17,40 @@ We do not want the task of checking and uploading the data to be onerous, and we
 
 st.subheader("Importance of checking your data")
 st.write("""
-Data will be uploaded through the data portal [here](#).
+Data will be uploaded through the data portal [here](https://test-bee-box.streamlit.app/~/+/Data_portal). Please check that the image associated with the data that you are uploading are correct; this will ensure that the data we collect over time for each hotel is accurate and correct.
 """)
 st.write("""
-Please check that the image associated with the data that you are uploading is correct.
+When you open the data portal, you will see a short list of drop downs. 
 """)
-st.write("""
-This will ensure that the data we collect over time for each hotel is accurate and correct.
-""")
-st.write("""
-When you open the data portal, you will see that there is a drop-down box with the hotel numbers that you have been assigned.
-""")
-st.write("""
-Please select the hotel number that you want to enter data for.
-""")
-st.write("""
-You will then see that there are a series of questions that you will be prompted to answer, some of which are mandatory. 
-""")
-st.write("""
-To ease data collection, a number of the answers will prefill with your last data entry. 
-""")
-st.write("""
-It is therefore very important to check the data you upload is correct. Please check this before submitting your data each time you upload an observation.
-""")
-st.write("""
-Once uploaded the submission number will be the same for the individual hotel over time, the observation number will be unique for the individual nest within the hotel. 
-""")
-st.write("""
+st.markdown("""
+1.	Firstly, you will need to select your name under "**Recorded by**" and then you will need to enter your unique passcode. This should be provided to you once you have provided your bee hotel initial collection data (see "Install your hotel").
+
+2. You must then choose from your bee hotels in the "**Hotel code**" drop down. This will pre-populate the data to be enterred for each hotel in terms of the holes that are present and also the last entry will be pre-populated so that you can simply update whatever has changed! 
+
+3. Now it's time to enter some data! There is a section of general data that will be associated with each row (nest hole).
+    
+    a. Please check the observation date and tiem are correct for when you took your photo. Please note that the time is in **24-hour format**. 
+
+    b. You can make some overall observations for your hotel as a whole if you'd like if you think it's interesting or necessary. These are written notes that could be of interest for every observation maybe things like "lots of Gasteruption parasitoid wasps present during the day", "Extremely hot weather today and bees were not active"...
+
+    c. Finally, there is a place to upload a single photo. It may be that we allow more photos in the future if the need arises (especially for ID purposes). However, for the moment we have limited storage.
+
+4. Now we can get to talking about the actual nest hole data. This might be a bit daunting but we have tried to keep it at least a little simple. For each nest you can provide data about:
+
+    a. The scientific name of the insect in the hotel. We have provided some "Identification resources" in the left-hand menu bar that are certainly worth exploring, but your core team member should be available to help as well with ID (see also "Collecting specimens"). Presently, there are a set number of species that you can enter; if you have new ones, please let James or Dezerae know and they can update this for you. Presently as well, only one entry is allowed in each cell and chat to us if you think that this should change.
+
+    b. The number of COMPLETED bee cells. These are the sealed-off cells that contain brood and provisions (baby bees/wasps and food). If there are false cells (empty ones) in-between, for the moment take note of this in the "Notes" column.
+
+    c. The number of adult males (♂️) and females (♀️). If you don't know the sex, then you can enter this in the "?" column.
+
+    d. Signs of sociality. You can select any number of these signs and we can add more as required. If the bee is alone you might say "**Solitary**", if there are more than one then you might say "**Social**", if there are parasites present you might say "**Parasitic**", and if you observe trophallaxis (baby-bird-like regurgitation) you can note that down as well. Then, there is opportunity to write some notes on this, if you'd like to provide extra context.
+
+Work your way down the list and then you can click "**Submit**", once you're certain that everything has been entered. If you make a mistake, that's okay, but it will need to be fixed manually in the back-end. It is therefore very important to check the data you upload is correct. Please check this before submitting your data each time you upload an observation.
+
 If you have made a mistake in the data you have uploaded, please get in contact with your contact person. 
+
+
 """)
+
+
 
